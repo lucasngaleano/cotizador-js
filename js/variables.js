bf0df9219e19672d46c6d6e3a216f23c0f1f283a
Lucas Galeano <lucas.n.galeano@gmail.com>
2024-07-03 22:55:07 -0300

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -1,1 +1,51 @@
+const datosPropiedas = [
+    {
+        tipo: 'casa',
+        factor: 1.009,
+    },
+    {
+        tipo: 'P.H.',
+        factor: 1.005,
+    },
+    {
+        tipo: 'Dto. Edificio',
+        factor: 1.002,
+    },
+    {
+        tipo: 'Barrio Privado',
+        factor: 1.019,
+    },
+    {
+        tipo: 'Oficina',
+        factor: 1.039,
+    },
+    {
+        tipo: 'Local comercial',
+        factor: 1.041,
+    },
+    {
+        tipo: 'Deposito logistica',
+        factor: 1.092,
+    },
+
+];
+const datosUbicacion = [
+    {
+        ubi: 'CABA',
+        factor: 1.013,
+    },
+    {
+        ubi: 'Tandil',
+        factor: 1.004,
+    },
+    {
+        ubi: 'Costa atlantica',
+        factor: 1.029,
+    },
+    {
+        ubi: 'Patagonia Argentina',
+        factor: 1.000,
+    },
+];
+
 
